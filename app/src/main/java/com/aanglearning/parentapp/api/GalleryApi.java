4542bc157293fec3688c5290f5d96b778988a6a9
vinkrish <vinaykrishna1989@gmail.com>
2017-11-08 15:06:11 +0530

    removed subalbum/images & related tables

--- app/src/main/java/com/aanglearning/parentapp/api/GalleryApi.java
+++ app/src/main/java/com/aanglearning/parentapp/api/GalleryApi.java
@@ -4,10 +4,6 @@ import com.aanglearning.parentapp.model.Album;
 import com.aanglearning.parentapp.model.AlbumImage;
 import com.aanglearning.parentapp.model.DeletedAlbum;
 import com.aanglearning.parentapp.model.DeletedAlbumImage;
-import com.aanglearning.parentapp.model.DeletedSubAlbum;
-import com.aanglearning.parentapp.model.DeletedSubAlbumImage;
-import com.aanglearning.parentapp.model.SubAlbum;
-import com.aanglearning.parentapp.model.SubAlbumImage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,33 +48,5 @@ public interface GalleryApi {
 
     @GET("deletedai/album/{albumId}")
     Call<List<DeletedAlbumImage>> getDeletedAlbumImages(@Path("albumId") long albumId);
-
-    @GET("subalbum/{id}/album/{albumId}")
-    Call<List<SubAlbum>> getSubAlbumAboveId(@Path("albumId") long albumId,
-                                            @Path("id") long id);
-
-    @GET("subalbum/album/{albumId}")
-    Call<List<SubAlbum>> getSubAlbums(@Path("albumId") long albumId);
-
-    @GET("deletedsubalbum/{id}/album/{albumId}")
-    Call<List<DeletedSubAlbum>> getDeletedSubAlbumsAboveId(@Path("albumId") long albumId,
-                                                           @Path("id") long id);
-
-    @GET("deletedsubalbum/album/{albumId}")
-    Call<List<DeletedSubAlbum>> getDeletedSubAlbums(@Path("albumId") long albumId);
-
-    @GET("sai/{id}/subalbum/{subAlbumId}")
-    Call<List<SubAlbumImage>> getSubAlbumImagesAboveId(@Path("subAlbumId") long subAlbumId,
-                                                       @Path("id") long id);
-
-    @GET("sai/subalbum/{subAlbumId}")
-    Call<List<SubAlbumImage>> getSubAlbumImages(@Path("subAlbumId") long subAlbumId);
-
-    @GET("deletedsai/{id}/subalbum/{subalbumId}")
-    Call<List<DeletedSubAlbumImage>> getDeletedSubAlbumImagesAboveId(@Path("subalbumId") long subalbumId,
-                                                                     @Path("id") long id);
-
-    @GET("deletedsai/subalbum/{subalbumId}")
-    Call<List<DeletedSubAlbumImage>> getDeletedSubAlbumImages(@Path("subalbumId") long subalbumId);
 
 }
